fbf08a17cb436668004f03fc9cf6dd9a7153fed8
ljcheng <ljcheng@thoughtworks.com>
2018-03-28 10:21:56 +0800

    add right title to browser

--- containers/Browser.js
+++ containers/Browser.js
@@ -1,37 +1,65 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { View, WebView, StyleSheet } from 'react-native';
+import { Text, View, WebView, TouchableOpacity, StyleSheet } from 'react-native';
 
 import { browser as mapStateToProps } from '../selectors';
 import { browser as browserActions } from '../actions';
 
 import NavigationBar from '../components/NavigationBar';
 
-const Browser = (props) => {
-  const { history, uri, title, loading, loadStart, loadDone, setTitle } = props;
-  const handleMessage = (event) => {
-    this.canGoBack = event.nativeEvent.canGoBack;
+class Browser extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      canGoBack: false,
+      canGoForward: false,
+    };
+  }
+
+  handleMessage = (event) => {
+    const { loadDone, setTitle } = this.props;
+    this.setState({ ...event.nativeEvent });
     setTitle(event.nativeEvent.data);
     loadDone();
   };
-  const handleBack = () => (
-    this.canGoBack ? (!loading && this.webView.injectJavaScript('window.history.back()')) : history.goBack()
-  );
-  return (
-    <View style={styles.container}>
-      <NavigationBar title={title} onBack={handleBack} />
-      <WebView
-        ref={ component => this.webView = component }
-        source={{ uri }}
-        onLoadStart={loadStart}
-        onLoadEnd={loadDone}
-        injectedJavaScript="window.postMessage(document.title)"
-        onMessage={handleMessage}
-        startInLoadingState
-      />
-    </View>
-  );
-};
+
+  handleBack = () => {
+    const { history, loading } = this.props;
+    const { canGoBack } = this.state;
+    return (
+      canGoBack ? (!loading && this.webView.injectJavaScript('window.history.back()')) : history.goBack()
+    );
+  };
+
+  renderRightTitle = () => {
+    const { loading } = this.props;
+    const { canGoForward } = this.state;
+    const handlePress = () => (!loading && this.webView.injectJavaScript('window.history.forward()'));
+    return canGoForward ? (
+      <TouchableOpacity onPress={handlePress} style={styles.rightTitle}>
+        <Text>Forward</Text>
+      </TouchableOpacity>
+    ) : <View />;
+  };
+
+  render() {
+    const { uri, title, loadStart, loadDone } = this.props;
+    return (
+      <View style={styles.container}>
+        <NavigationBar title={title} onBack={this.handleBack} renderRightTitle={this.renderRightTitle} />
+        <WebView
+          ref={ component => this.webView = component }
+          source={{ uri }}
+          onLoadStart={loadStart}
+          onLoadEnd={loadDone}
+          injectedJavaScript="window.postMessage(document.title)"
+          onMessage={this.handleMessage}
+          startInLoadingState
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -40,6 +68,10 @@ const styles = StyleSheet.create({
   fetchingIndicator: {
     flex: 0,
     height: 64,
+  },
+  rightTitle: {
+    alignSelf: 'stretch',
+    justifyContent: 'center',
   },
 });
 
